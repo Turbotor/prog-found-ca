c7ccb879d776a6c85022bff02c516b2ca856626f
Turbotor <63958625+Turbotor@users.noreply.github.com>
2020-06-07 20:07:40 +0200

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,63 @@
+//Q1
+var Boxvar = ["brown", "red","green"]
+//Q2 
+var person = {
+Name: "Franklin",
+Age: 50
+};
+//Q3 
+var  OutOfStock = true;
+if (OutOfStock) {
+console.log("Out Of Stock");
+} else {
+console.log("In Stock");
+}
+//Q4
+var numberArray = ["One", "two", "three", "four", "five"];
+for (var i = 0; i < numberArray.length; i++) {
+console.log(numberArray[i].One);
+console.log(numberArray[i].Two);
+console.log(numberArray[i].Three);
+console.log(numberArray[i].Four);
+console.log(numberArray[i].Five);
+}
+//Q5
+for (var i = 15; i <= 25; i += 1) {
+console.log(i);
+}
+//Q6
+for (var i = 15; i <= 25; i += 20) {
+console.log(i);
+}
+//Q7
+var animalArray = ["Cat","Dog"]
+var animalArray = [
+{
+Cat: "Joe"
+colorWhite = true;
+Age: 5
+Dog: "Nora"
+colorbrown = true;
+Age 7
+var animalArray = ["Cat,"Dog"];
+for (var i = 0; i < animalArray.length; i++) {
+console.log(animaArray[i]5 ;
+console.log(animaArray[i]7 ;
+console.log(animaArray[i]colorwhite= true ; 
+console.log(animaArray[i]colorbrown= true ; 
+}
+//Q8
+function whatIDontLike(Candy) {
+var IDontLike = "Candy";
+console.log("Candy");
+}
+whatIdontLike(Candy);
+//Q10
+var clothingArray
+function TshirtColor(TshirtGreen); {
+clothingArray.push("Tshirtgreen");
+}
+function Tshirtcolor(Tshirtgreen) {
+console.log("green");
+}
+Tshirtcolor(Green);
